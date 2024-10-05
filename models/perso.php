8821ff04bddf8a308c92d226a12c148ab5661f03
kikisan <kiketdule@gmail.com>
2024-10-05 16:06:12 +0200

    ok

--- models/perso.php
+++ models/perso.php
@@ -3,10 +3,9 @@
 
 //  pour la connexion à hostinger
 define("mysql","89.116.147.103" );
-define("dbname","u256533777_phpmvcpoo" );
-define("user","u256533777_phpmvcpoo" );
-define("mdpbd","*Santa30420*" );
-
+define("dbname","u256533777_phpmvcpootest" );
+define("user","u256533777_phpmvcpootest" );
+define("mdpbd","Mdpphpmvcpoo2024" );
 
 //  pour connexion à db locale
 // define("mysql","localhost" );
